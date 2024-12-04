5ec41c6f702c5f2026865657e03a5ed5cac4a3c1
Naman Goyal <namangoyal.com@gmail.com>
2024-12-04 11:58:46 +0530

    Completed the uc2

--- Uc1.js
+++ Uc1.js
@@ -1,12 +1,41 @@
+// Use to Calculate Attenddence
 function checkattendence(){
+    //Generate a random number
     let employee = Math.floor(Math.random()*11);
-    console.log(employee);
     if(employee < 1){
         console.log("Employee is Absent");
     }
     else{
         console.log("Employee is Present");
+        calculatewage();
     }
 }
-
 checkattendence();
+
+// Use to calculate the daily wage
+function calculatewage(){
+    // const array of valid numbers
+    const numbers = [0,1,4,8];
+    const randomIndex = Math.floor(Math.random()*numbers.length);
+    let hour = numbers[randomIndex];
+    //console.log(hour);
+    let Totalwage = 0;
+    switch (hour){
+        case 0:
+            Totalwage = 0;
+            console.log("He did not do work "+ Totalwage);
+            break;
+        case 1:
+            Totalwage = Totalwage+20;
+            console.log("He is per hour worker and his salary is : " + Totalwage );
+            break;
+        case 4:
+            Totalwage = Totalwage +(4*20);
+            console.log ("He is a part time worker and his salary is: "+Totalwage);
+            break;
+        case 8:
+            Totalwage = Totalwage + (8*20);
+            console.log ("He is a full time worker and his salary is: "+ Totalwage);
+            break;
+    }
+}
